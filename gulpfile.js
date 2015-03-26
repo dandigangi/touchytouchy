474a545a05482142f7c9d1ae028a54081cdb4609
Dan DiGangi <dandigangi@Dans-MacBook-Pro.local>
2015-03-26 18:50:48 -0400

    Initial commit. Gulp tasks set.

---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+/*!
+ * gulp
+ * $ npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
+ */
+
+var del = require('del');
+
+// Gulp Plugins
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var notify = require('gulp-notify');
+var cache = require('gulp-cache');
+var livereload = require('gulp-livereload');
+
+// CSS
+gulp.task('css', function() {
+  return sass('src/assets/css/app.scss', {
+      style: 'expanded'
+    })
+    .pipe(
+      autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')
+    )
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(notify({
+      message: 'CSS build complete!'
+    }));
+});
+
+// Images (CSS)
+gulp.task('images', function() {
+  return gulp.src('src/assets/css/images/**/*')
+    .pipe(cache(imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/assets/css/images'))
+    .pipe(notify({
+      message: 'CSS image compressions complete!'
+    }));
+});
+
+// Javascript
+gulp.task('javascript', function() {
+  return gulp.src('src/assets/javascript/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('dist/assets/javascript'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/assets/javascript'))
+    .pipe(notify({
+      message: 'Javascript build complete!'
+    }));
+});
+
+// Images (Main)
+gulp.task('images', function() {
+  return gulp.src('src/assets/images/**/*')
+    .pipe(cache(imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/assets/images'))
+    .pipe(notify({
+      message: 'Main image compressions complete!'
+    }));
+});
+
+// Cleanup
+gulp.task('clean', function(cb) {
+  del(['dist/assets/css', 'dist/assets/javascript', 'dist/assets/images'], cb)
+});
+
+// Builder
+gulp.task('default', ['clean'], function() {
+  gulp.start('css', 'javascript', 'images');
+});
+
+// Watchers
+gulp.task('watch', function() {
+  gulp.watch('src/assets/css/**/*.scss', ['styles']);
+  gulp.watch('src/assets/javascript/**/*.js', ['scripts']);
+  gulp.watch('src/assets/images/**/*', ['images']);
+  livereload.listen();
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+});
---
+++ src/assets/javascript/app.js
@@ -0,0 +1,11 @@
+var generateHexColor = function() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+};
+
+var updateColorScheme = function() {
+  var newColorScheme = generateHexColor();
+  touchyApp.style.backgroundColor = newColorScheme;
+};
+
+var touchyApp = document.getElementById('touchy-app');
+touchyApp.addEventListener('click', updateColorScheme);
