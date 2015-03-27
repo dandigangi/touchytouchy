dca6a24c4e0c099e374ba1cb06629a81ab3b211f
Dan DiGangi <dandigangi@Dans-MacBook-Pro.local>
2015-03-26 21:02:20 -0400

    Updates to ignore, added p package.json with devDepends, added vendor styles normalize but still investigating import issue

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 /*!
  * gulp
- * $ npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
+ * $ npm install del gulp-ruby-sass gulp-sourcemaps gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache --save-dev
  */
 
+// Modules
 var del = require('del');
 
-// Gulp Plugins
+// Gulp
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+// Gulp Plugins
 var autoprefixer = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
 var jshint = require('gulp-jshint');
@@ -22,7 +26,10 @@ var livereload = require('gulp-livereload');
 // CSS
 gulp.task('css', function() {
   return sass('src/assets/css/app.scss', {
-      style: 'expanded'
+      sourcemap: true,
+      style: 'expanded',
+      stopOnError: true,
+      loadPath: 'src/assets/css'
     })
     .pipe(
       autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')
@@ -73,7 +80,7 @@ gulp.task('javascript', function() {
 gulp.task('images', function() {
   return gulp.src('src/assets/images/**/*')
     .pipe(cache(imagemin({
-      optimizationLevel: 3,
+      optimizationLevel: 4,
       progressive: true,
       interlaced: true
     })))
@@ -95,9 +102,6 @@ gulp.task('default', ['clean'], function() {
 
 // Watchers
 gulp.task('watch', function() {
-  gulp.watch('src/assets/css/**/*.scss', ['styles']);
-  gulp.watch('src/assets/javascript/**/*.js', ['scripts']);
-  gulp.watch('src/assets/images/**/*', ['images']);
   livereload.listen();
   gulp.watch(['dist/**']).on('change', livereload.changed);
 });
